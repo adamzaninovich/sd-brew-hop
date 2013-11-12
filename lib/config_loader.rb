aaf5cf385d8d34014ebdda5346a3b4e2699c79c2
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-11-12 15:46:51 -0800

    fix config loader

--- lib/config_loader.rb
+++ lib/config_loader.rb
@@ -1,6 +1,6 @@
 class ConfigLoader
   def self.load_yml_config file, env
-    config = YAML.load_file("#{Rails.root}#{file}").with_indifferent_access
+    config = YAML.load_file(File.join Rails.root, file).with_indifferent_access
     raise "Missing '#{env}' config in #{file}" if config[env].blank?
     config[env]
   end
