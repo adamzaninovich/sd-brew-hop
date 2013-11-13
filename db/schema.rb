285940e50762594061d550d7c5a01ab6baad2e5c
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-11-13 08:24:41 -0800

    Revert "adds paperclip to brewery table"
    This reverts commit a492d935eaffbcc2fa1d0633e8b3b9d207d866d3.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131113155403) do
+ActiveRecord::Schema.define(version: 20131113061042) do
 
   create_table "breweries", force: true do |t|
     t.string   "name"
@@ -20,10 +20,6 @@ ActiveRecord::Schema.define(version: 20131113155403) do
     t.integer  "year_opened"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
-    t.datetime "photo_updated_at"
   end
 
   create_table "users", force: true do |t|
