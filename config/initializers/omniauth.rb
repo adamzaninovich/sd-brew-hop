ebf66a5eccd54af9245ab0fb79bf38ae5125bce5
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-11-15 21:25:23 -0800

    it helps to use the correct class name for OmniAuth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-Omniauth.config.logger = Rails.logger
+OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
