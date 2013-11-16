669e6fb245be66a545dad44906a4883fd8bcc0d2
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-11-15 20:22:59 -0800

    removes devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 SdBrewHop::Application.routes.draw do
 
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-
   resources :breweries
 
   get "legal/terms"   => 'home#terms',   as: :terms
