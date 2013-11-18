684cf7b853587ddde209fe086ee9e60deb550cc1
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-11-18 09:16:57 -0800

    fixes error and annotates user model and spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,13 +7,15 @@ end
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  provider   :string(255)
-#  uid        :string(255)
-#  name       :string(255)
-#  email      :string(255)
-#  image      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id               :integer          not null, primary key
+#  provider         :string(255)
+#  uid              :string(255)
+#  name             :string(255)
+#  email            :string(255)
+#  image            :string(255)
+#  created_at       :datetime
+#  updated_at       :datetime
+#  oauth_token      :string(255)
+#  oauth_expires_at :datetime
 #
 
