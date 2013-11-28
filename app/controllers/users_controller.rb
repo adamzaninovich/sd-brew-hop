dbb266f51b903b6f30852dd97635a9ddc0a5e1b5
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-11-28 12:43:50 -0800

    remove auth on brewery index and show pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
-  before_filter :authorize!
 
   def show
     @user = if params[:id]
       User.find params[:id]
     else
-      current_user
+      current_user or authorize! and return
     end
   end
 end
